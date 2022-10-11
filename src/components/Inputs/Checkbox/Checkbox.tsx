4eba85dc9bd698c1931c396368b4d71dc2d1f978
yiwmsh <lonelevelsands@gmail.com>
2022-10-10 21:53:32 -0600

    ✨ Checkbox!

--- src/components/Inputs/Checkbox/Checkbox.tsx
+++ src/components/Inputs/Checkbox/Checkbox.tsx
@@ -4,6 +4,7 @@ import {
   useTransform,
 } from "framer-motion/dist/framer-motion";
 import React from "react";
+import styled from "@emotion/styled";
 import {
   AriaCheckboxProps,
   useCheckbox,
@@ -11,6 +12,29 @@ import {
   VisuallyHidden,
 } from "react-aria";
 import { useToggleState } from "react-stately";
+import { SemanticColors } from "../../../types/Color";
+
+const Checkmark = styled(motion.path)`
+  stroke: var(${SemanticColors.primary});
+`;
+
+const Box = styled(motion.path)`
+  stroke: var(${SemanticColors.text});
+
+  &:hover,
+  :active {
+    stroke: var(${SemanticColors.primary});
+  }
+`;
+
+const CheckmarkContainer = styled(motion.svg)`
+  cursor: pointer;
+  stroke: var(${SemanticColors.text});
+
+  :hover > ${Box} {
+    stroke: var(${SemanticColors.primary});
+  }
+`;
 
 const boxPath =
   "M 5.94 2.7 C 4.32 2.7 2.7 4.32 2.7 5.94 V 15.66 C 2.7 17.28 4.32 18.9 5.94 18.9 H 15.66 C 17.28 18.9 18.9 17.28 18.9 15.66 V 5.94 C 18.9 4.32 17.28 2.7 15.66 2.7 H 5.85";
@@ -52,7 +76,7 @@ export const Checkbox: React.FC<AriaCheckboxProps> = (props) => {
       <VisuallyHidden>
         <input {...inputProps} ref={ref} />
       </VisuallyHidden>
-      <motion.svg
+      <CheckmarkContainer
         width={24}
         height={24}
         aria-hidden="true"
@@ -62,21 +86,20 @@ export const Checkbox: React.FC<AriaCheckboxProps> = (props) => {
         whileTap="pressed"
         whileHover="hover"
       >
-        <motion.path
+        <Box
           d={boxPath}
           fill="transparent"
           strokeWidth="2"
           stroke="#000000"
           variants={boxVariants}
         />
-        <motion.path
+        <Checkmark
           d={checkPath}
           strokeWidth={2}
           fill="transparent"
-          stroke="black"
           variants={checkVariants}
         />
-      </motion.svg>
+      </CheckmarkContainer>
       {children}
     </label>
   );
