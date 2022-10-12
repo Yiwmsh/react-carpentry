13cfed1dadbe8d19e5a8944692a8e557d7894963
yiwmsh <lonelevelsands@gmail.com>
2022-10-12 15:09:04 -0600

    🚧

--- src/consts/measurements.ts
+++ src/consts/measurements.ts
@@ -1,3 +1,7 @@
 export const BORDER_ROUNDING = "0.375em";
 
 export const CARD_CONTENT_VERTICAL_MARGIN = "0.25em";
+
+export const SCROLL_BAR_WIDTH = "--scrollBarWidth";
+
+export const VIEW_PORT_WIDTH = "--viewportWidth";
