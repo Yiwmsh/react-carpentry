31afb87036fbe064502576e5947a9b9050e8c8da
yiwmsh <lonelevelsands@gmail.com>
2022-10-13 18:39:21 -0600

    🐛CSS fixes

--- src/components/Containers/Modal/Modal.tsx
+++ src/components/Containers/Modal/Modal.tsx
@@ -19,16 +19,18 @@ interface ModalProps extends AriaOverlayProps {
 }
 
 const Underlay = styled.div`
-  position: "fixed";
+  position: fixed;
   z-index: 100;
+  width: 100%;
+  height: 100%;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
-  background: "rgba(0, 0, 0, 0.5)";
-  display: "flex";
-  align-items: "center";
-  justify-content: "center";
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 /* 
