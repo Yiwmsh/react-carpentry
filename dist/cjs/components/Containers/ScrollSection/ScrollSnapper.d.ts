5f056a33c9029afa633c1bb693120d3d375e0aaa
Christopher Ellis <lonelevelsands@gmail.com>
2022-12-02 21:24:04 -0700

    🧱⬆ Version and build (#29)

--- dist/cjs/components/Containers/ScrollSection/ScrollSnapper.d.ts
+++ dist/cjs/components/Containers/ScrollSection/ScrollSnapper.d.ts
@@ -2,4 +2,6 @@
 export declare const ScrollSnapper: import("@emotion/styled").StyledComponent<{
     theme?: import("@emotion/react").Theme | undefined;
     as?: import("react").ElementType<any> | undefined;
+} & {
+    hideOverflow?: boolean | undefined;
 }, import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, {}>;
