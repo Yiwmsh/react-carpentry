103dea5ca5e05969b65e47e6416c67869f70d1c9
Christopher Ellis <lonelevelsands@gmail.com>
2022-12-02 21:17:26 -0700

    ✨ ScrollSnapper optional hide overflow (#27)

--- src/components/Containers/ScrollSection/ScrollSnapper.tsx
+++ src/components/Containers/ScrollSection/ScrollSnapper.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
-export const ScrollSnapper = styled.div`
-  overflow-y: auto;
+export const ScrollSnapper = styled.div<{ hideOverflow?: boolean }>`
+  overflow-y: ${({ hideOverflow }) => (hideOverflow ? "hidden" : "auto")};
   overscroll-behavior-y: contain;
   scroll-snap-type: y mandatory;
   height: 100vh;
